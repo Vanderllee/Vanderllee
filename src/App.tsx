a57628920f039fb960de86637164c056c4f94cc4
Vanderllee JS <vanderllee@gmail.com>
2021-04-06 10:03:26 -0300

    add fonts and styles to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <h1>Welcome to my new Vanderlle SITE </h1>;
-};
+import { GlobalStyle } from './styles/GlobalStyles';
 
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Sejam muuuuito bem vindos ao meu site!</h1>
+      <br />
+      <p>Meu nome é José Vanderli</p>
+      <br />
+      <a href="https://www.instagram.com/vanderllee/">@Vanderllee</a>
+      <br />
+      <a href="https://www.github.com/vanderllee">Vanderllee</a>
+      <GlobalStyle />
+    </>
+  );
+};
 export default App;
