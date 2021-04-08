b23f216d55c4001304799c1ddcd82cd61d3ee571
Vanderllee JS <vanderllee@gmail.com>
2021-04-08 11:28:15 -0300

    added MenuDrawer to project

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import { FiSun } from 'react-icons/fi';
+import React, { useState, useCallback } from 'react';
+import { FiSun, FiMenu, FiX } from 'react-icons/fi';
 
-import { Background, Container } from './styles';
+import { Background, Container, MenuDrawer } from './styles';
 
 const NavBar: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = useCallback((): void => {
+    setOpen(!open);
+  }, [open]);
+
   return (
-    <Background>
-      <Container>
-        <p>Vanderllee</p>
-        <ul>
-          <FiSun size={28} color="#D4AE8B" />
-          <li>Cursos</li>
-          <li>Portfolio</li>
-          <li>Contato</li>
-        </ul>
-      </Container>
-    </Background>
+    <>
+      <Background>
+        <Container>
+          <p>Vanderllee</p>
+          <FiMenu size={28} color="#fff" onClick={handleOpen} />
+          <ul>
+            <FiSun size={28} color="#D4AE8B" />
+            <li>Cursos</li>
+            <li>Portfolio</li>
+            <li>Contato</li>
+          </ul>
+        </Container>
+        <MenuDrawer isOpen={open}>
+          <FiX size={28} color="#fff" onClick={handleOpen} />
+          <ul>
+            <li>Cursos</li>
+            <li>Portfolio</li>
+            <li>Contato</li>
+            <FiSun size={28} color="#D4AE8B" />
+          </ul>
+        </MenuDrawer>
+      </Background>
+    </>
   );
 };
 
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,29 +1,82 @@
 import styled from 'styled-components';
 
+interface MenuDrawerProps {
+  isOpen: boolean;
+}
+
 export const Background = styled.div`
+  position: relative;
   background-color: #272727;
   color: #fff;
 `;
+
 export const Container = styled.div`
   margin: auto;
   width: 80vw;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+  position: relative;
   p {
     font-family: 'Anton', Arial, Helvetica, Sans-serif;
     font-size: 30px;
     color: #bb9878;
   }
   li {
-    font-family: 'Anton', Arial, Helvetica, Sans-serif;
     text-transform: uppercase;
-    font-size: 30px;
-    padding-left: 25px;
+    margin-left: 25px;
+    font-family: 'Anton', Arial, Helvetica, Sans-serif;
   }
   ul {
     display: flex;
     flex-direction: row;
     align-items: center;
+  }
+  > SVG {
+    display: none;
+  }
+  @media (max-width: 700px) {
+    width: 90vw;
+    > SVG {
+      display: block;
+    }
+
+    ul {
+      display: none;
+    }
+  }
+`;
+
+export const MenuDrawer = styled.ul<MenuDrawerProps>`
+  position: absolute;
+  background-color: #272727;
+  top: 0px;
+  height: 100vh;
+  right: 0;
+  display: ${(props): string => (props.isOpen ? 'block' : 'none')};
+
+  svg:first-child {
+    height: 44px;
+    margin-left: calc(90% - 24px);
+  }
+
+  li {
+    text-transform: uppercase;
+    padding: 10px 0 10px 25px;
+    border-bottom: solid 1px #737373;
+    font-family: 'Anton', Arial, Helvetica, Sans-serif;
+
+    & + svg {
+      margin: 20px 0 0 25px;
+    }
+  }
+
+  @media (max-width: 700px) {
+    width: 50vw;
+  }
+
+  @media (max-width: 460px) {
+    width: 70vw;
   }
 `;
